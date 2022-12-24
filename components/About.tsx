2d098ca609821ef5474eaa4455eb367d43fe449b
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2022-12-24 21:16:14 +0000

    Add about section

---
+++ components/About.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+
+type Props = {
+  pageInfo: PageInfo;
+};
+
+function About({ pageInfo }: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      //   viewport={{ once: true }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 h-screen justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{
+          x: -200,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmGkX2UMeuIauZ1oyEOaWbavdFxuhA3q3O18ZMMta4Bj7V_BXkB6nrQ37P12f9VQ5QoiQ&usqp=CAU"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
+      ></motion.img>
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base">lorem ipsum</p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,11 @@ export default function Home() {
 
       <Header />
 
-      <section id="hero">
+      <section id="hero" className="snap-start">
         <Hero />
+      </section>
+      <section id="about" className="snap-center">
+        <About />
       </section>
     </div>
   );
