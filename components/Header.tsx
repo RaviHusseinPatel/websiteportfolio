ea7980c060d08a89eab6b909fc778ab0e23891ae
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2022-12-24 21:02:26 +0000

    NPM:Next,Tail CSS, Framer motion, react icons, typewriter; Create Header&Hero section

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+type Props = {};
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 flex items-start xl:items-center justify-between p-5 z-20 max-w-7xl mx-auto">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
+        
+          <SocialIcon
+           url="https://www.linkedin.com/in/ravihussein-patel/"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+       <SocialIcon
+           url="https://github.com/RaviHusseinPatel"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+      </motion.div>
+
+      
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{ duration: 1 }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/MotionCircles.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+function MotionCircles() {
+  return (
+    <motion.div
+      className="relative flex justify-center items-center"
+      initial={{ opacity: 0 }}
+      transition={{ duration: 2.5 }}
+      whileInView={{ opacity: 1 }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+    >
+      <div className="rounded-full border border-[#333333] h-[200px] w-[200px] absolute mt-52 animate-ping" />
+      <div className="rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52" />
+      <div className="rounded-full border border-[#333333] h-[500px] w-[500px] absolute mt-52" />
+      <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse" />
+      <div className="rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52" />
+    </motion.div>
+  );
+}
+
+export default MotionCircles;
