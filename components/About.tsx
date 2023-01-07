b1375b030882fd28c9e2fd1a60bc085062cbcf1b
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2023-01-07 03:55:08 +0000

    add xyz

--- components/About.tsx
+++ components/About.tsx
@@ -26,17 +26,17 @@ function About() {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmGkX2UMeuIauZ1oyEOaWbavdFxuhA3q3O18ZMMta4Bj7V_BXkB6nrQ37P12f9VQ5QoiQ&usqp=CAU"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
+        src="https://previews.dropbox.com/p/thumb/ABzmWELgJytRyGpL_sBUdqpAH4kaVhiv3BhHQpi-kHAy1GWmDmrRWlH26Ja-Ze5lY4qU7aW58bpKvZ_bMf63EAiMOL2wPmWL1WerDuByi88Dl0ukuhLT5LOZt8V2hXNmhT_kh4ttg6QZxOdIqLqG7rb0J2zMrEBbXwr3X3hN-PnQ0uCldI1_IBPmRGQ2SgxU4ZEMYnn1ilyH4s19tu-cJFHZaKEHdUNfr1ASPEfteNbtejh9Bz9ZZyysk2ypgYjKNMxiwtwtPcnKCu1ufVozHN2j1IWPOZ7fGzEYZe7Fs8hCQixp9wjHxN0jHVnyBF-zuWtvUVZNibZnvYoAJ2-rWjrAt_xv3pad679Ka1k5Ap4im_86p7jNK4sPtlpdNcxFtOSir9WbBwSWrA80nJDIwt5HFewyXFvg-vUnQ3T4DEFYEJeTNUZOYDh42IclgIkRfwqpvASPA9u63durMLXhXz5dBdR8_H2Lgbbof7MZnQTiXZ3eJwMIiguEQIQWDivU9rLugSyHIpveE2r1e0BHBK48ikKgrLDJWpQ-JnvjNTx0qn9AuVWAeCUd1o-lYOMZjNo/p.png"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[300px] xl:h-[400px] object-cover"
       ></motion.img>
 
       <div className="space-y-10 px-0 md:px-10">
         <h4 className="text-4xl font-semibold">
           Here is a{" "}
-          <span className="decoration-[#0a79f7]/50 underline">little</span>{" "}
+          <span className="decoration-[#8A9A5B]/50 underline">little</span>{" "}
           background
         </h4>
-        <p className="text-lg">As an aspiring React developer with a year of experience building backend and frontend APIs, I have strong communication skills and a deep understanding of how to work with others. My experience in client-facing roles and as a tutor has given me the ability to ask the right questions and learn from others, which is a crucial skill for any developer.
+        <p className="text-sm xl:text-lg">As an aspiring React developer with a year of experience building backend and frontend APIs, I have strong communication skills and a deep understanding of how to work with others. My experience in client-facing roles and as a tutor has given me the ability to ask the right questions and learn from others, which is a crucial skill for any developer.
             I was able to demonstrate these skills during the BrightNetwork Technology Academy, where our group's final projects received recognition for being both creative and efficient.
             <br></br> <br></br>
             <b>Programming is important because it allows us to harness the power of engineering to improve the lives of people on a daily basis. 
