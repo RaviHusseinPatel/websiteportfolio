e04afd16d29527ca51d1f1887e64cbcd47011009
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2023-01-08 23:51:13 +0000

    small css

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -38,7 +38,7 @@ function Contact({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-   className="flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 h-screen justify-evenly mx-auto items-center justify-center">
+   className="flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 h-screen mx-auto items-center justify-center">
   <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
     Contact
   </h3>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -46,7 +46,7 @@ function Projects({}: Props) {
       transition={{ duration: 1.5 }}
       className="flex relative overflow-hidden flex-col text-left md:flex-row max-w-full h-screen justify-evenly mx-auto items-center z-0"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-12 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
       <br></br>
