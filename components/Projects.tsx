abe328eaa8bfb551dbf1103ae586ed9860fd4e01
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2023-01-18 23:53:19 +0000

    add keys to maps

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { VscSourceControl } from "react-icons/vsc";
-import worldpin from "../public/worldpin.png";
 
 type Props = {};
 
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -106,24 +106,26 @@ function Skill({ directionLeft }: Props) {
 
   return (
     <>
-      {skills.map(({ id, image, proficiency }) => (
-        <div className="group relative flex cursor-pointer">
-          <motion.img
-            initial={{
-              x: directionLeft ? -200 : 500,
-            }}
-            className="rounded-full border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale grayscale-0 transition duration-300 ease-in-out"
-            transition={{ duration: 1 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            src={image}
-          />
+      {skills.map(({ skill,id,image,proficiency }) => (
+        <div key={id}>
+          <div className="group relative flex cursor-pointer">
+            <motion.img
+              initial={{
+                x: directionLeft ? -200 : 500,
+              }}
+              className="rounded-full border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale grayscale-0 transition duration-300 ease-in-out"
+              transition={{ duration: 1 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              src={image}
+            />
 
-          <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
-            <div className="flex items-center justify-center h-full">
-              <p className="text-3xl font-bold text-black opacity-100">
-                {proficiency}
-              </p>
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+              <div className="flex items-center justify-center h-full">
+                <p className="text-3xl font-bold text-black opacity-100">
+                  {proficiency}
+                </p>
+              </div>
             </div>
           </div>
         </div>
