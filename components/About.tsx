22b3fd928fd96d667f8941acdc00652e70718604
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2023-01-18 23:21:24 +0000

    about

--- components/About.tsx
+++ components/About.tsx
@@ -24,7 +24,7 @@ function About() {
         whileInView={{opacity:1,x:0}}
         viewport={{once:true}}
         src="https://previews.dropbox.com/p/thumb/ABwrdBLt6x7GCGlCMIcDs21Y8EOFrIKsnZs2WZmMlrsuWUEKKpbf6xhbTOkQkvmQTTo0tOpz54QdG5J59awZSqTOOWKRz5zDnA5DCuN-GgYEQsgVVwvxpiDTdBe0hWoFEIDcx971zOBkRGeI16pH-DTf42MB7jF68DGcOPscMpDETjtmR4rI0ujux55Razr5G9EA82PkeLm7ezTYprNqyRKfV2vKrMPcT1qKIbcNP14mz_IcvPuLiapxL3q4JmIx-e1Vuqy-3LvQH7H4GrSCClqgYoctyq0z8hQBHoS0DIq30JYrIYfgfd89VbxK_pPt3I4wbUmPRfH7wm4DvE573fMTx2DoMu_ZsL7eWFvffohRSYrm-I2-e19yqhPs_q9m0h2d0mG0rtVMtmXKQfwvCHWskkF1oKpgyO5ltpewyjUVQzjFQ8kGWjQyLrx_cgb5P6xmUs8aXNaEaIWgMQoWrPeRXYT_igMDM15KG7Uhuwu4wBlld5eoFK6BuMrC_8uoPYpxGTijcOd7ZwQ3BCtC2CrAmXGLoje_OQhBonNQ4yexniHKzW9zGDSZW5Sd8aTzA8U/p.png"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-{300px} xl:h-{400px} object-cover"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-(300px) xl:h-(400px) object-cover"
       ></motion.img>
 
       <div className="space-y-10 px-0 md:px-10">
