a10bccfce7d8f3fedfedf1617d698a79a2ec0734
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2023-01-19 16:00:53 +0000

    format far right skill column to load in

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -109,7 +109,7 @@ function Skill({ directionLeft }: Props) {
           <div className="group relative flex cursor-pointer">
             <motion.img
               initial={{
-                x: directionLeft ? -200 : 500,
+                x: directionLeft ? -100 : 300,
               }}
               className="rounded-full border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale grayscale-0 transition duration-300 ease-in-out"
               transition={{ duration: 1 }}
